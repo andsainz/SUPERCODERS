0e410c855f877ccab424d64fd27f7a1fa4ee818c
andsainz <dreasvi@gmail.com>
2023-07-11 14:11:47 +0200

    quedarse con la versión que ha subido jaime

--- supercoder.js
+++ supercoder.js
@@ -1,3 +1,4 @@
+let coders = ["Cyntia", "Diego A", "Fernando", "Geovanny", "Gisela", "Jaime", "Jassed", "Jes", "Jimmy", "Jorge", "Pedro", "Raúl", "Rubén", "Sophia", "Thuanny", "Virginia", "Adriana", "Luis", "Ainhoa", "Andrea", "Diego B", "Emily", "Víctor", "Wanda", "Bryan"]
 let botonElem = document.getElementById('change--name')
 let citaElem = document.getElementById('random--name')
 
