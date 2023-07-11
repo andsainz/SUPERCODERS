c1d12b4c87d7faf36344b95dffbffe8de774a6ba
jaime martinez romero <jimi_jr_9@hotmail.com>
2023-07-11 14:05:15 +0200

    reset button ok

--- supercoder.js
+++ supercoder.js
@@ -1,5 +1,7 @@
+let coders = ["Cyntia", "Diego A", "Fernando", "Geovanny", "Gisela", "Jaime", "Jassed", "Jes", "Jimmy", "Jorge", "Pedro", "Raúl", "Rubén", "Sophia", "Thuanny", "Virginia", "Adriana", "Luis", "Ainhoa", "Andrea", "Diego B", "Emily", "Víctor", "Wanda", "Bryan"]
 let botonElem = document.getElementById('change--name')
-let citaElem = document.getElementById('random--name')
+let namesElem = document.getElementById('random--name')
+
 
 function random(min, max){
     return Math.floor(Math.random() * (max - min) + min);
@@ -7,9 +9,19 @@ function random(min, max){
 
 function cambiarNombre(){
     let randomIndex = random(0, coders.length)
-    citaElem.innerText = coders[randomIndex]
+    namesElem.innerText = coders[randomIndex]
 }
 
 cambiarNombre();
 
 botonElem.addEventListener('click', cambiarNombre);
+
+let botonReset = document.getElementById('reset');
+
+function resetearCampo() {
+    namesElem.innerText = "";
+}
+
+botonReset.addEventListener('click', function() {
+    resetearCampo();
+});
