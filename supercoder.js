8026749c3750a87c60e101ad2decc15cce6a6fe6
jaime martinez romero <jimi_jr_9@hotmail.com>
2023-07-24 14:50:01 +0200

    cambios javascript

--- supercoder.js
+++ supercoder.js
@@ -14,24 +14,33 @@ buttonReset.addEventListener("click", function(){
     
 })
 
-function showNames(){
-    let list = document.querySelector("#namesList");
-    list.innerHTML = "";
-    for (let i = 0; i < superCoders.length; i++){
-        let coder = superCoders[i];
-        let li = document.createElement("li");
-        li.textContent = coder;
-        list.appendChild(li);
-    }
-}
-showNames();
-
 function anotherCoder(){
     let newName = document.querySelector("#newName").value 
     superCoders.push(newName);
     document.querySelector("#newName").value = "";
     showNames()
 };
+
+
+function showNames(){
+    let list = document.querySelector("#namesList");
+    list.innerHTML = "";
+    for (let i = 0; i < superCoders.length; i++){
+    let li = document.createElement("li");
+    li.innerHTML = `<span>${superCoders[i]}</span><img src="images/draw.png" onclick="editName(${i})"> <img src="images/close.png" onclick="removeName(${i})">`;
+    list.appendChild(li);
+    
+    }
+};
+
+function editName(index){
+    let namePrompt = prompt("Edita el nombre del superCoder", superCoders[index]);
+    superCoders[index] =  namePrompt    
+    showNames();
+}
+showNames()
+
+
 
 
 
