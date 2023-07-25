c799caf420a04bfb00865a8285359d250e513e9e
catari22 <catarigeovanny@gmail.com>
2023-07-25 11:59:05 +0200

    pagina de inicio

--- supercoder.js
+++ supercoder.js
@@ -1,14 +1,18 @@
 const superCoders = [ "Cyntia", "Diego A", "Fernando", "Geovanny", "Gisela", "Jaime", "Jassed", "Jes", "Jimmy", "Jorge", "Pedro", "Raúl", "Rubén", "Sophia", "Thuanny", "Virginia", "Adriana", "Luis", "Ainhoa", "Andrea", "Diego B", "Emily", "Víctor", "Wanda", "Bryan"];
 
 function changeName() {
-    let index = Math.floor(Math.random() * superCoders.length)
-    let coderName = superCoders[index]
-    superCoders.splice(index, 1) //no vuelve a aparecer el coder seleccionado
+    if (superCoders.length <= 0) {
+        alert("El último Coder ya ha sido seleccionado."); // Alerta cuando es el último coder
+        window.location.href = "coders-home.html"; // Volver a la pantalla de inicio
+        return;
+    }
+
+    let index = Math.floor(Math.random() * superCoders.length);
+    let coderName = superCoders[index];
+    superCoders.splice(index, 1); // No volverá a aparecer el coder seleccionado
 
     document.getElementById("random--name").textContent = coderName;
-    if(superCoders.length<=0){
-        alert("último coder") //alerta último coder
-    } else{alert(`${coderName}, te tocó`)}//aviso del nombre del coder seleccionado
+    alert(`${coderName}, te tocó`); // Aviso del nombre del coder seleccionado
 }
 
 //agregar nombres
